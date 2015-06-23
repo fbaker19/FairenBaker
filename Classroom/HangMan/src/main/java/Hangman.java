5f887ce079fecc6fb9eb7e7913e5fba52f5acfe8
Fairen Baker <f.baker91@gmail.com>
2015-06-22 22:25:19 -0400

    update

--- Classroom/HangMan/src/main/java/Hangman.java
+++ Classroom/HangMan/src/main/java/Hangman.java
@@ -1,5 +1,5 @@
 
-
+import com.swcguild.hangman.ConsoleIO;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -16,13 +16,11 @@ import java.util.Random;
 public class Hangman {
 
     public static void main(String[] args) {
-        
-        ConsoleIO io = new ConsoleIO();
+
+        ConsoleIO con = new ConsoleIO();
 
         StringBuilder builder = new StringBuilder();
         Random r = new Random();
-        
-       
 
         boolean done = false;
 
@@ -49,17 +47,17 @@ public class Hangman {
             int tries = (chosenWord.length()) * 2;
 
             while (tries > 0) {
-                io.printStr("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-                io.printStr("");
+                 con.readString("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                 con.readString("");
                 System.out.print("Word:     ");
                 for (int j = 0; j < dashes.length; j++) {
                     System.out.print(dashes[j] + " ");
                 }
-                io.printStr("\n");
-                io.printStr("Misses:   " + misses);
-                io.printStr("");
-                String guess = io.readSingleLetter("Guess:    ");
-                io.printStr("");
+                con.readString("\n");
+                con.readString("Misses:   " + misses);
+                con.readString("");
+                String guess = con.readSingleLetter("Guess:    ");
+                con.readString("");
 
                 boolean guessCorrect = false;
                 for (int i = 0; i < wordArray.length; i++) {
@@ -71,7 +69,7 @@ public class Hangman {
                 }
 
                 if (guessCorrect) {
-                    io.printStr("You guessed correctly!");
+                    con.readString("You guessed correctly!");
                 } else {
                     misses = misses + guess;
                 }
@@ -79,27 +77,27 @@ public class Hangman {
                 answer = Arrays.toString(dashes).replace(",", "").replace("[", "").replace("]", "").trim();
 
                 if (chosenWord.equals(answer.replaceAll("\\s", ""))) {
-                    io.printStr("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-                    io.printStr("");
+                    con.readString("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                    con.readString("");
                     System.out.print("Word:     ");
                     for (int j = 0; j < dashes.length; j++) {
                         System.out.print(dashes[j] + " ");
                     }
-                    io.printStr("\n");
-                    io.printStr("Misses:   " + misses);
-                    io.printStr("");
-                    io.printStr("Great job!  You win!\n");
+                    con.readString("\n");
+                    con.readString("Misses:   " + misses);
+                    con.readString("");
+                    con.readString("Great job!  You win!\n");
                     tries = 0;
                 }
 
                 tries--;
             }
-            io.printStr("The word was: " + answer + "\n");
-            if (io.readYorN("Play again? [y/n]").equalsIgnoreCase("n")) {
+             con.readString("The word was: " + answer + "\n");
+            if (con.readYorN("Play again? [y/n]").equalsIgnoreCase("n")) {
                 done = true;
             }
         }
-        io.printStr("GAME OVER");
+        con.readString("GAME OVER");
     }
 
 }
